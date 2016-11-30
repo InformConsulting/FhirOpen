653251d6cb7f61049ad5ed359ae0a6b7889f9cb9
Kirill Kazachenko <kazachenkoks@gmail.com>
2016-11-30 14:22:25 +0600

    secound commit

--- client.py
+++ client.py
@@ -1,11 +1,17 @@
 import requests
 import json
 
-FHIR_URL = 'http://fhirtest.uhn.ca/baseDstu2/Patient'
+#FHIR_URL = 'http://fhirtest.uhn.ca/baseDstu2/Patient'
+FHIR_URL = 'https://argonaut.aidbox.io/fhir/Patient'
 
 
 def list():
     r = requests.get(FHIR_URL, dict(_format='json', _pretty=True))
+    return r.json()
+
+
+def read(pk):
+    r = requests.get('{}/{}'.format(FHIR_URL, pk), dict(_format='json', _pretty=True))
     return r.json()
 
 
@@ -32,4 +38,14 @@ def create():
     r.headers = {'Accept': 'application/json+fhir', "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
     return r.json()
 
-print(create())
+
+def delete(pk):
+    r = requests.delete('{}/{}'.format(FHIR_URL, pk))
+    r.headers = {'Accept': 'application/json+fhir'}
+
+    return r
+
+
+#print(create())
+#print(delete('5fb02143-cc23-4eb2-a07d-4f68c745840d'))
+print(read('5fb02143-cc23-4eb2-a07d-4f68c745840d'))
