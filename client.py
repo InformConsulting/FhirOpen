5be903afac5eacd2eb33c8ddcdd641dda2da59c3
Kirill Kazachenko <kazachenkoks@gmail.com>
2016-11-30 13:22:01 +0600

    secound commit

--- client.py
+++ client.py
@@ -1,6 +1,35 @@
 import requests
+import json
 
-url = 'http://fhirtest.uhn.ca/baseDstu2/Patient'
-r = requests.get(url, dict(_format='json', _pretty=True))
+FHIR_URL = 'http://fhirtest.uhn.ca/baseDstu2/Patient'
 
-print(r.json())
+
+def list():
+    r = requests.get(FHIR_URL, dict(_format='json', _pretty=True))
+    return r.json()
+
+
+def create():
+    data = {
+        "resourceType": "Patient",
+        "name": [
+            {
+                "family": [
+                    "Fletchers"
+                ],
+                "given": [
+                    "Arturo",
+                    "F"
+                ]
+            }
+        ],
+        "gender": "unknown",
+        "birthDate": "1942-01-28",
+        "deceasedBoolean": False
+    }
+
+    r = requests.post(FHIR_URL, json.dumps(data))
+    r.headers = {'Accept': 'application/json+fhir', "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"}
+    return r.json()
+
+print(create())
